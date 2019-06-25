85283a348b868c9ef786a782be4af6a6a9ec5894
GulshanN <nihalani_gulshan@yahoo.com>
2019-06-25 21:35:16 +0530

    First Commit

---
+++ ext-modules/psFramework/psFrameworkDirective.js
@@ -0,0 +1,14 @@
+(function() {
+    "use strict";
+
+    angular.module('psFramework').directive('psFramework',function(){
+        return{
+            transclude: true,
+            scope:{
+
+            },
+            controller: "psFrameworkController",
+            templateUrl: 'ext-modules/psFramework/psFrameworkTemplate.html'
+        };
+    })
+})();
---
+++ ext-modules/psFramework/psFrameworkModule.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('psFramework', ['psMenu','psDashboard'
+            
+        ]);
+
+}());
